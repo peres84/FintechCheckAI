65d9430632fd942ad47fb724bb88ed1126bc99c9
Rouxxel <106406623+Rouxxel@users.noreply.github.com>
2026-01-24 19:54:40 +0100

    config and custom logger set up

--- backend/core/config.py
+++ backend/core/config.py
@@ -1,8 +1,33 @@
-from pydantic import BaseModel
+import json
+import sys
 
+def read_data_from_json(file_path: str, exit_on_error: bool = True) -> dict:
+    """
+    Reads data from a JSON configuration file.
 
-class Settings(BaseModel):
-    app_env: str = "development"
+    Returns:
+        dict: Parsed JSON configuration data.
+    """
+    try:
+        with open(file_path, "r", encoding="utf-8") as file:
+            config_data = json.load(file)
+        return config_data
+    except FileNotFoundError:
+        print(f"ERROR: Config file not found: {file_path}")
+        if exit_on_error:
+            sys.exit(1)
+        else:
+            return None
+    except json.JSONDecodeError:
+        print(f"ERROR: Failed to parse JSON config file: {file_path}")
+        if exit_on_error:
+            sys.exit(1)
+        else:
+            return None
 
+"""VARIABLES-----------------------------------------------------------"""
+#Path to your config JSON file
+CONFIG_FILE_PATH = "backend/core/config.json"
 
-settings = Settings()
+#Load the entire configuration data, the one to be used
+config = read_data_from_json(CONFIG_FILE_PATH, exit_on_error=True)
--- backend/core/logging.py
+++ backend/core/logging.py
@@ -1,5 +1,56 @@
+import os
 import logging
+import sys
+import datetime
 
+#Other files imports
+from backend.core.config import config
 
-def configure_logging() -> None:
-    logging.basicConfig(level=logging.INFO, format="%(levelname)s %(name)s %(message)s")
+#Map config string levels to logging module levels
+LOG_LEVELS = {
+    "critical": logging.CRITICAL,
+    "error": logging.ERROR,
+    "warning": logging.WARNING,
+    "info": logging.INFO,
+    "debug": logging.DEBUG,
+    "notset": logging.NOTSET
+}
+
+#Get log level string from config and convert to logging level, default to INFO if not found
+log_level_str = config["logging"].get("logging_level", "info").lower()
+log_level = LOG_LEVELS.get(log_level_str, logging.INFO)
+
+"""Log basic configuration"""
+log_handler = logging.getLogger(config["logging"]["log_file_name"])
+log_handler.setLevel(log_level)
+
+"""Logger formatter"""
+log_format = logging.Formatter(
+    fmt="%(asctime)s %(msecs)03dZ | %(levelname)s | %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S"
+)
+
+"""File handler (File accessible only when it runs locally)"""
+#Create folder
+log_directory = config["logging"]["dir_name"]
+os.makedirs(log_directory,exist_ok=True)
+
+#Create log file
+log_file = os.path.join(
+                    log_directory, 
+                    datetime.datetime.now().strftime(
+                        f"{config["logging"]["log_file_name"]}_%Y-%m-%dT%H-%M-%S.log"))
+file_handler = logging.FileHandler(log_file)
+file_handler.setFormatter(log_format)
+
+"""Console handler for Render logs"""
+console_handler = logging.StreamHandler(sys.stdout)
+console_handler.setFormatter(log_format)
+
+#Final log handler
+if not log_handler.hasHandlers():
+    log_handler.addHandler(file_handler)
+    log_handler.addHandler(console_handler)
+
+log_handler.info(f"Simple Chat backend server starting")
+log_handler.warning(f"Current working directory: {os.getcwd()}, Logs are written to '{log_file}'")
